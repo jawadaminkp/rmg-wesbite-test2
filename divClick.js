eaaee8ef47746bda2dd53c6ec0121e6eb74af3a9
Jawad Amin <59015763+jawadaminkp@users.noreply.github.com>
2021-11-01 13:32:08 +0500

    Add files via upload

---
+++ divClick.js
@@ -0,0 +1,55 @@
+const card_1 = document.querySelector('.card-1');
+
+const card_2 = document.querySelector('.card-2');
+
+const card_3 = document.querySelector('.card-3');
+
+const card_4 = document.querySelector('.card-4');
+
+const card_5 = document.querySelector('.card-5');
+
+const card_6 = document.querySelector('.card-6');
+
+
+card_1.addEventListener("click",open_service_1);
+
+
+function open_service_1(){
+    window.location.href = './service-1.html';
+}
+
+card_2.addEventListener("click",open_service_2);
+
+
+function open_service_2(){
+    window.location.href = './service-2.html';
+}
+
+card_3.addEventListener("click",open_service_3);
+
+
+function open_service_3(){
+    window.location.href = './service-3.html';
+}
+
+card_4.addEventListener("click",open_service_4);
+
+
+function open_service_4(){
+    window.location.href = './service-4.html';
+}
+
+card_5.addEventListener("click",open_service_5);
+
+
+function open_service_5(){
+    window.location.href = './service-5.html';
+}
+
+card_6.addEventListener("click",open_service_6);
+
+
+function open_service_6(){
+    window.location.href = './service-6.html';
+}
+
